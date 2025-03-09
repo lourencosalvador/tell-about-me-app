c9df32ab05f065c27a3fc3544d11dfb15b53ed3c
Paulo Sanguli <paulosanguli@gmail.com>
2025-03-10 00:28:57 +0100

    app: init application setup

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+
+export default function TestCom() {
+    return (
+        <View className="flex-1 bg-black">
+            <Text className="text-white m-8 bg-yellow">Hello word!</Text>
+        </View>
+    )
+}
